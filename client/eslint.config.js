8641c1c51aed713948f109b6e8ce8321292bc6e3
Leif Ershag <leif@ershag.se>
2026-03-25 21:29:23 +0100

    Build fix.

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -19,6 +19,8 @@ export default [
         console:         "readonly",
         setTimeout:      "readonly",
         clearTimeout:    "readonly",
+        setInterval:     "readonly",
+        clearInterval:   "readonly",
         URL:             "readonly",
         WebSocket:       "readonly",
         fetch:           "readonly",
